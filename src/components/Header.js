68c0e20bb1f883c74710383636f5ca12aa48322d
Agung Fadhil <agung.fadhil@telkom.co.id>
2021-08-14 20:16:47 +0700

    setup react-router-dom and Profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <header className="absolute w-full z-50 px-4">
@@ -16,24 +18,36 @@ function Header() {
               id="menu"
             >
               <li className="mx-3 py-6 md:py-0">
-                <a href="/" className="text-black md:text-white hover:underline">
+                <a
+                  href="/"
+                  className="text-black md:text-white hover:underline"
+                >
                   Showcase
                 </a>
               </li>
               <li className="mx-3 py-6 md:py-0">
-                <a href="/" className="text-black md:text-white hover:underline">
+                <a
+                  href="/"
+                  className="text-black md:text-white hover:underline"
+                >
                   Catalog
                 </a>
               </li>
               <li className="mx-3 py-6 md:py-0">
-                <a href="/" className="text-black md:text-white hover:underline">
+                <a
+                  href="/"
+                  className="text-black md:text-white hover:underline"
+                >
                   Delivery
                 </a>
               </li>
               <li className="mx-3 py-6 md:py-0">
-                <a href="/" className="text-black md:text-white hover:underline">
-                  Rewards
-                </a>
+                <Link
+                  to="/profile"
+                  className="text-black md:text-white hover:underline"
+                >
+                  Profile
+                </Link>
               </li>
             </ul>
           </div>
