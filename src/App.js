ccfe1bd4997c340a21206f0e874b82f35af2bb53
Agung Fadhil <agung.fadhil@telkom.co.id>
2021-08-14 18:58:22 +0700

    add splash screen

--- src/App.js
+++ src/App.js
@@ -7,56 +7,71 @@ import Clients from "./components/Clients";
 import AsideMenu from "./components/AsideMenu";
 import Footer from "./components/Footer";
 import Offline from "./components/Offline";
+import Splash from "./pages/Splash";
 
 function App() {
   const [items, setItems] = React.useState([]);
   const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   function handleOfflineStatus() {
     setOfflineStatus(!navigator.onLine);
   }
 
-  React.useEffect(function () {
-    (async function () {
-      const response = await fetch(
-        "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            accept: "application/json",
-            "x-api-key": process.env.REACT_APP_APIKEY,
-          },
-        }
-      );
-      const { nodes } = await response.json();
-      setItems(nodes);
+  React.useEffect(
+    function () {
+      (async function () {
+        const response = await fetch(
+          "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              accept: "application/json",
+              "x-api-key": process.env.REACT_APP_APIKEY,
+            },
+          }
+        );
+        const { nodes } = await response.json();
+        setItems(nodes);
 
-      const script = document.createElement("script");
-      script.src = "/carousel.js";
-      script.async = false;
-      document.body.appendChild(script);
-    })();
+        const script = document.createElement("script");
+        script.src = "/carousel.js";
+        script.async = false;
+        document.body.appendChild(script);
+      })();
 
-    handleOfflineStatus();
-    window.addEventListener('online', handleOfflineStatus);
-    window.addEventListener('offline', handleOfflineStatus);
+      handleOfflineStatus();
+      window.addEventListener("online", handleOfflineStatus);
+      window.addEventListener("offline", handleOfflineStatus);
 
-    return function() {
-      window.removeEventListener('online', handleOfflineStatus);
-      window.removeEventListener('offline', handleOfflineStatus);
-    }
-  }, [offlineStatus]);
+      setTimeout(function () {
+        setIsLoading(false);
+      }, 1500);
+
+      return function () {
+        window.removeEventListener("online", handleOfflineStatus);
+        window.removeEventListener("offline", handleOfflineStatus);
+      };
+    },
+    [offlineStatus]
+  );
 
   return (
     <>
-    {offlineStatus && <Offline />}
-      <Header />
-      <Hero />
-      <Browse />
-      <Arrived items={items} />
-      <Clients />
-      <AsideMenu />
-      <Footer />
+      {isLoading ? (
+        <Splash />
+      ) : (
+        <>
+          {offlineStatus && <Offline />}
+          <Header />
+          <Hero />
+          <Browse />
+          <Arrived items={items} />
+          <Clients />
+          <AsideMenu />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
